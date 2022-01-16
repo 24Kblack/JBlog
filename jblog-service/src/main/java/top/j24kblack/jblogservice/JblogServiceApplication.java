58434a2abc62511a3625bae623506c924515d740
24Kblack <my24kblack@gmail.com>
2022-01-16 14:48:40 +0800

    本次提交初始化了项目，使用的技术栈有springboot、mybatis-plus、vue+vuetify

---
+++ jblog-service/src/main/java/top/j24kblack/jblogservice/JblogServiceApplication.java
@@ -0,0 +1,13 @@
+package top.j24kblack.jblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JblogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JblogServiceApplication.class, args);
+    }
+
+}
---
+++ jblog-service/src/test/java/top/j24kblack/jblogservice/JblogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.j24kblack.jblogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JblogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
